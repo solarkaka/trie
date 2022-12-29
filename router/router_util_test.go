e11e86eaa3f7d6d431d093fc1d4776411a988a8f
zc272184 <zc272184@alibaba-inc.com>
2022-12-29 11:01:54 +0800

    rename func

--- router/router_util_test.go
+++ router/router_util_test.go
@@ -9,22 +9,22 @@ func TestUtil(t *testing.T) {
 	util := NewRouteUtil()
 	var hosts = []string{"b.keruyun.com"}
 	var paths = []string{"/**"}
-	util.parseHostPath(hosts, paths, 1)
+	util.ParseHostPath(hosts, paths, 1)
 	var hosts2 = []string{"b.keruyun.com"}
 	var paths2 = []string{"/aa/**"}
-	util.parseHostPath(hosts2, paths2, 2)
+	util.ParseHostPath(hosts2, paths2, 2)
 	var hosts3 = []string{"b.keruyun.com"}
 	var paths3 = []string{"/aa/ab/**"}
-	util.parseHostPath(hosts3, paths3, 3)
+	util.ParseHostPath(hosts3, paths3, 3)
 	var hosts4 = []string{"b.keruyun.com"}
 	var paths4 = []string{"/aa/ab"}
-	util.parseHostPath(hosts4, paths4, 4)
+	util.ParseHostPath(hosts4, paths4, 4)
 	var hosts5 = []string{"kpib.keruyun.com"}
 	var paths5 = []string{"/aa/**"}
-	util.parseHostPath(hosts5, paths5, 5)
+	util.ParseHostPath(hosts5, paths5, 5)
 	var hosts6 = []string{"b.keruyun.com"}
 	var paths6 = []string{"/aa/ab/ac/**", "//aaa"}
-	util.parseHostPath(hosts6, paths6, 6)
+	util.ParseHostPath(hosts6, paths6, 6)
 
 	matchService(1, "b.keruyun.com/aa", *util, t)
 	matchService(1, "b.keruyun.com/ab", *util, t)
